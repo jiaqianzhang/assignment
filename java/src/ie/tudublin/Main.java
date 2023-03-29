280203ac560dfe8233a059613e020a36c8b7b4bf
jiaqianzhang <jiaqianlyrb@gmail.com>
2023-03-29 13:02:07 +0100

    assignment

---
+++ java/src/ie/tudublin/Main.java
@@ -0,0 +1,21 @@
+package ie.tudublin;
+
+import example.CubeVisual;
+import example.MyVisual;
+import example.RotatingAudioBands;
+
+public class Main
+{	
+
+	public void startUI()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new MyVisual());		
+	}
+
+	public static void main(String[] args)
+	{
+		Main main = new Main();
+		main.startUI();			
+	}
+}
