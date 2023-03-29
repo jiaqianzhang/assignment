80131a6c50bcdf2af352e5bdf1575fb4faa54fd9
jiaqianzhang <jiaqianlyrb@gmail.com>
2023-03-29 15:05:13 +0100

    assignment

--- java/src/ie/tudublin/MusicVisual.java
+++ java/src/ie/tudublin/MusicVisual.java
@@ -1,8 +1,55 @@
 package ie.tudublin;
 
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
 import processing.core.PApplet;
 
 public class MusicVisual extends PApplet
 {
+    Minim minim;
+    AudioPlayer ap;
+    AudioBuffer ab;
+
+    int mode = 0;
+
+    public void keyPressed() 
+    {
+        if (key >= '0' && key <= '9') 
+        {
+			mode = key - '0';
+		}
+		if (keyCode == ' ') 
+        {
+            if (ap.isPlaying()) 
+            {
+                ap.pause();
+            } 
+            else 
+            {
+                ap.rewind();
+                ap.play();
+            }
+        }
+	}
+
+    public void settings()
+    {
+    }
+
+    public void setup()
+    {
+        minim = new Minim(this);
+        // Uncomment this to use the microphone
+        // ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
+        // ab = ai.mix; 
+
+        // And comment the next two lines out
+        ap = minim.loadFile("Believer.mp3", 1024);
+        ap.play();
+        ab = ap.mix;
+        colorMode(HSB);
+     
+    }
 
 }
